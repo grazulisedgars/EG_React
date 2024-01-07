e0782c1ed5e2b4945426724e623f2358d2e48e9a
grazulisedgars <grazulisedgars@gmail.com>
2024-01-07 17:35:56 +0200

    Adds styles and background photo to Home Page

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "../HomePage/HomePage.css"
 
 function HomePage() {
     return (
-        <div className="p-5 mb-4 bg-body-tertiary rounded-3">
-            <div className="container-fluid py-5">
+        <div className="custom-p-5 mb-4" id="jumbotron">
+            <div className="container-fluid custom-py-5">
                 <h1 className="display-5 fw-bold">Edgars Grazulis</h1>
-                <h2 className="col-md-8 fs-4">React Showcase Portfolio</h2>
-                <p className="col-md-8 fs-4">My experience in website building extends beyond platforms like Squarespace, Wix and Wordpress</p>
+                <h2 className="custom-col-md-8 custom-fs-4">React Showcase Portfolio</h2>
+                <p className="custom-col-md-8 c-fs-4">My experience in website building extends beyond platforms like Squarespace, Wix and Wordpress</p>
             </div>
         </div>
     );
