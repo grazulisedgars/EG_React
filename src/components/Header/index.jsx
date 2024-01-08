7be6cd09a847e8dc83bbc14c376c0c13bc6944bd
grazulisedgars <grazulisedgars@gmail.com>
2024-01-08 16:08:02 +0200

    Adds functionality for Header. Able to change between classes now

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,21 +5,34 @@ import "../Header/Header.css"
 function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
-            <a className="navbar-brand" href="#">Logo</a>
+            <a className="navbar-brand">EG</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                <Link to="/EG_React" className="nav-item nav-link active">
-            Home
-          </Link>
-          <Link to="/projectspage" className="nav-item nav-link">
-            Projects
-          </Link>
-          <Link to="/contact" className="nav-item nav-link">
-            Contact
-          </Link>
+                    
+                    <Link
+                        to="/EG_React"
+                        className={`nav-item nav-link ${location.pathname === "/EG_React" ? "active" : ""
+                            }`}
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        to="/projectspage"
+                        className={`nav-item nav-link ${location.pathname === "/projectspage" ? "active" : ""
+                            }`}
+                    >
+                        Projects
+                    </Link>
+                    <Link
+                        to="/contact"
+                        className={`nav-item nav-link ${location.pathname === "/contact" ? "active" : ""
+                            }`}
+                    >
+                        Contact
+                    </Link>
                 </div>
             </div>
         </nav>
