be070e2e1013c0130b91fd1dabfab6c06ef9579e
grazulisedgars <grazulisedgars@gmail.com>
2024-01-08 14:58:32 +0200

    Adds projects to projects page

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,74 @@ import ContactPage from "./components/ContactPage";
 import Projects from "./components/Projects";
 import projects from './data/projectsData.json';
 
-function DefaultLayout ({children}) {
+function DefaultLayout({ children }) {
   return (
     <>
-    <Header />
-    {children}
+      <Header />
+      {children}
     </>
   );
 }
 
 
 function App() {
-    return (
-      <Router>
+  return (
+    <Router>
       <Routes>
         <Route path="/EG_React" element={
-            <DefaultLayout>
-              <HomePage />
-            </DefaultLayout>
-          }
+          <DefaultLayout>
+            <HomePage />
+          </DefaultLayout>
+        }
         />
         <Route path="projectspage/" element={
           <DefaultLayout>
-            <ProjectsPage />
+            <ProjectsPage>
+              <Projects
+                title={projects[0].title}
+                screenshot={projects[0].screenshot}
+                deployLink={projects[0].deployLink}
+                githubLink={projects[0].githubLink}
+              />
+              <Projects
+                title={projects[1].title}
+                screenshot={projects[1].screenshot}
+                deployLink={projects[1].deployLink}
+                githubLink={projects[1].githubLink}
+              />
+              <Projects
+                title={projects[2].title}
+                screenshot={projects[2].screenshot}
+                deployLink={projects[2].deployLink}
+                githubLink={projects[2].githubLink}
+              />
+              <Projects
+                title={projects[3].title}
+                screenshot={projects[3].screenshot}
+                deployLink={projects[3].deployLink}
+                githubLink={projects[3].githubLink}
+              />
+              <Projects
+                title={projects[4].title}
+                screenshot={projects[4].screenshot}
+                deployLink={projects[4].deployLink}
+                githubLink={projects[4].githubLink}
+              />
+              <Projects
+                title={projects[5].title}
+                screenshot={projects[5].screenshot}
+                deployLink={projects[5].deployLink}
+                githubLink={projects[5].githubLink}
+              />
+            </ProjectsPage>
           </DefaultLayout>} />
         <Route path="contact/" element={
-        <DefaultLayout>
+          <DefaultLayout>
             <ContactPage />
           </DefaultLayout>} />
       </Routes>
     </Router>
-    )
+  )
 }
 
 export default App;
