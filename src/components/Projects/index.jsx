42e7cd8399d6c6fcfd7b9ea265002ae5ac787371
grazulisedgars <grazulisedgars@gmail.com>
2024-01-09 13:26:22 +0200

    Adds styling to Projects page all screens

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../Projects/Project.css"
 
 function FriendCard(props) {
   return (
--- src/components/ProjectsPage/index.jsx
+++ src/components/ProjectsPage/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "../ProjectsPage/ProjectsPage.css"
 
 function ProjectsPage(props) {
 
     return (
         <>
-        <h2>Projects Page</h2>
-        <div className="wrapper">{props.children}</div>
+        <h2 className="projects-page-h2">Projects Page</h2>
+        <div className="projects-page">{props.children}</div>
         </>
     )
   }
